c12521a21d4f8882c3a64cfd5f7b9681e82a9fde
Ashish Sharma <sharmaashish049@gmail.com>
2022-12-30 22:03:03 +0530

    Day 24 Completed

---
+++ Day24/Day24.py
@@ -0,0 +1,13 @@
+# It is Day 24 of 100DaysOfCode with CodeWithHarry
+# Tuples [Tuples are non changeable]
+
+tup = (1, 5, 6)
+print(type(tup), tup)
+
+# There is a confusion in Python Developers mentioned below
+tup2 = (1)
+print(type(tup2))  # The output will be an type int ( to avoid this mistake we can add a comma if we are crearing single element typle like below)
+tup3 = (1,)
+print(type(tup3))  # This will print a type tuple 😇
+
+# Tuples are behave like the same as lists in python but tuples are non changeable and starts with a () bracket notation
