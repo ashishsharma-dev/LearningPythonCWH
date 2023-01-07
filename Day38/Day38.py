afef7abd48cc44335c0a70e1100d636617097a4a
Ashish Sharma <sharmaashish049@gmail.com>
2023-01-07 10:14:43 +0530

    Day 38 Completed with quick quiz

--- Day38/Day38.py
+++ Day38/Day38.py
@@ -4,7 +4,31 @@
 # Lets take a situation that we want to raise an error so that we will not messed up with the further code of our application ans stop the execution then we can use custom errors
 
 # Example given below: 
-a = int(input("Enter any number between 5 and 9"))
+# a = int(input("Enter any number between 5 and 9"))
 
-if(a<5 or a>9):
-    raise ValueError("Value should be between 5 and 9")
+# if(a<5 or a>9):
+#     raise ValueError("Value should be between 5 and 9")
+
+# Quick Quiz
+# Write a program to not raise an error if user types quit all caps
+
+# userInput = int(input("Enter a number to proceed: "))
+# print(userInput>5)
+# print(userInput<5)
+# print(userInput>9)
+# print(userInput<9)
+
+a=input("Enter the number: ")
+
+
+try:
+    if(int(a)>5 and int(a)<9):
+        print("It is between 5 and 9")
+    else:
+        print("Invalid Data")
+except:
+    print("Invalid Data")
+
+finally:
+    if(a.lower()=='quit'):
+        print("Quitting Now")
